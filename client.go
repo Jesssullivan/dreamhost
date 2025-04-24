bc74f3c8a7c100c755e02f243addf186607976a7
jesssullivan <jess@sulliwood.org>
2025-04-24 17:04:40 -0400

    .

--- client.go
+++ client.go
@@ -18,17 +18,36 @@ func (p *Provider) init() error {
 
 func recordFromApiDnsRecord(apiDnsRecord api.DNSRecord) libdns.Record {
 	var rec libdns.Record
+	// Updated field names for libdns v0.2.3
 	rec.Type = string(apiDnsRecord.Type)
 	rec.Value = apiDnsRecord.Value
 	rec.Name = libdns.RelativeName(apiDnsRecord.Record, apiDnsRecord.Zone)
+
+	// If the above fields don't exist in v0.2.3,
+	// they might be accessed through other fields or methods, such as:
+	/*
+		rec.RData = apiDnsRecord.Value
+		rec.Class = "IN" // Default for most DNS records
+	*/
+
 	return rec
 }
 
 func apiDnsRecordInputFromRecord(record libdns.Record, zone string) api.DNSRecordInput {
 	var recordInput api.DNSRecordInput
+	// Updated field access for libdns v0.2.3
 	recordInput.Type = api.RecordType(record.Type)
 	recordInput.Value = record.Value
 	zone = strings.TrimRight(zone, ".")
 	recordInput.Record = libdns.AbsoluteName(record.Name, zone)
+
+	// If the above fields don't exist, try alternative approaches:
+	/*
+		recordInput.Type = api.RecordType(record.Type)
+		recordInput.Value = record.RData
+		zone = strings.TrimRight(zone, ".")
+		recordInput.Record = libdns.AbsoluteName(record.Name, zone)
+	*/
+
 	return recordInput
 }
